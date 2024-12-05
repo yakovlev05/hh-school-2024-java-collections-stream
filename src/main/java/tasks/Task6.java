1ca48c3cb6f01f733cda8b87b87d54e78cc4b2da
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-05 22:21:10 +0500

    task6

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,10 +2,11 @@ package tasks;
 
 import common.Area;
 import common.Person;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -19,6 +20,15 @@ public class Task6 {
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, String> areaIdName = areas.stream()
+        .collect(Collectors.toMap(Area::getId, Area::getName));
+
+    return persons.stream()
+        .flatMap(p -> personAreaIds.get(p.id())
+            .stream()
+            .map(a -> "%s - %s".formatted(
+                p.firstName(),
+                areaIdName.get(a))))
+        .collect(Collectors.toSet());
   }
 }
