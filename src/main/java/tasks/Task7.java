4698bfe340bc3b4c06425f8f4f5623033b8bb275
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-05 22:22:31 +0500

    task7

--- src/main/java/tasks/Task7.java
+++ src/main/java/tasks/Task7.java
@@ -1,9 +1,11 @@
 package tasks;
 
 import common.Company;
+import common.Vacancy;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Из коллекции компаний необходимо получить всевозможные различные названия вакансий
@@ -11,7 +13,10 @@ import java.util.Set;
 public class Task7 {
 
   public static Set<String> vacancyNames(Collection<Company> companies) {
-    return new HashSet<>();
+    return companies.stream()
+        .flatMap(c -> c.getVacancies().stream()
+            .map(Vacancy::getTitle))
+        .collect(Collectors.toSet());
   }
 
 }
